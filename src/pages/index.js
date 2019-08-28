9303831369b9dd653d282153ce94f2d149befd0b
chubbymaus <court@chubbymaus.com>
2019-08-28 11:52:19 -0600

    window fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -94,7 +94,7 @@ class App extends React.Component {
   }
 
   moveSectionDown() {
-    if (window) {
+    if (typeof window !== `undefined`) {
       window.fullpage_api.moveSectionDown()
     }
   }
